1014f28a9acfbdda1dad91b60783a24da92d0827
Terry Li <terryseanli@gmail.com>
2023-10-04 16:55:01 -0700

    Adds entity ruler for jurisdictions and span ruler for defined terms, business types and incoterms.

--- pipe.py
+++ pipe.py
@@ -8,14 +8,53 @@ from preambles import preambles
 
 nlp = spacy.load("en_core_web_sm")
 
-jdx_ruler = nlp.add_pipe("entity_ruler", before="ner")
-state_matcher = PhraseMatcher(nlp.vocab)
-state_patterns = [{
-    "label": "JDX",
-    "pattern": [{"TEXT": {"IN": STATES}}]
-}]
-jdx_ruler.add_patterns(state_patterns)
+########## Labelling entities ##########
+
+entity_ruler = nlp.add_pipe("entity_ruler", before="ner")
+
+# Add more patterns for other ents here
+jdx_patterns = [{"label": "JDX", "pattern": [{"TEXT": {"IN": STATES}}]}]
+entity_patterns = jdx_patterns
+entity_ruler.add_patterns(entity_patterns)
 
 doc = nlp(preambles[0])
 print([(ent.text, ent.label_) for ent in doc.ents])
 
+
+########## Labelling spans ##########
+
+ruler = nlp.add_pipe("span_ruler", before="ner")
+
+# Matches ASCII tokens enclosed in quotation marks
+defined_term_patterns = [
+    {"label": "DEFTERM",
+     "pattern": [
+         {"ORTH": '“'},
+         {"IS_ASCII": True, "OP": "+"},
+         {"ORTH": '”'}
+     ]},
+    {"label": "DEFTERM",
+     "pattern": [
+         {"ORTH": '"'},
+         {"IS_ASCII": True, "OP": "+"},
+         {"ORTH": '"'}
+     ]},
+]
+
+biz_type_patterns = [
+    {"label": "BIZ_TYPE", "pattern": [{"TEXT": {"IN": BIZ_TYPES}}]},
+]
+
+incoterm_patterns = [
+    {"label": "INCOTERM", "pattern": [{"TEXT": {"IN": INCOTERMS}}]},
+]
+
+span_patterns = [
+    *defined_term_patterns,
+    *biz_type_patterns,
+    *incoterm_patterns
+]
+ruler.add_patterns(span_patterns)
+
+doc = nlp(preambles[0])
+print([(span.text, span.label_) for span in doc.spans["ruler"]])
