f809f3ea2d179b88f9fb82bc2181d31218cc1a4e
Terry Li <terryseanli@gmail.com>
2023-10-11 22:15:39 -0700

    Sanitized party names in example preambles and tested other currencies besides USD.

--- data/preambles.py
+++ data/preambles.py
@@ -1,15 +1,15 @@
 preambles=[
     """THIS MODULE SUPPLY AGREEMENT (this “Agreement”) is entered into as of July
-    15,2020 (“Effective Date”), between 325MK 8ME LLC, a state of Delaware limited
-    liability company (“Buyer”), and CANADIAN SOLAR (USA) INC., a
-    Delaware corporation (“Seller”).  Buyer and Seller are referred to herein
-    individually as a “Party”, and collectively as the “Parties”. This Agreement
-    shall be for $1,234 of value.
+    15,2020 (“Effective Date”), between 678M TEST LLC, a state of Delaware limited
+    liability company (“Buyer”), and GOOGOL (USA) INC., a Delaware corporation
+    (“Seller”).  Buyer and Seller are referred to herein individually as a
+    “Party”, and collectively as the “Parties”. This Agreement shall be for
+    ¥1,234, or £1,234 of value.
     """,
 
     """
-    This letter agreement is between Silicon Valley Accountancy Corporation, a
-    California corporation (“we”, “us,” or “our”), and Canadian Solar (USA)
+    This letter agreement is between Silicon Valley Professional Services, a
+    California corporation (“we”, “us,” or “our”), and Renewable Energy (USA)
     Inc., a Delaware corporation (“you,” “your,” or the “Company”), and sets
     forth the terms and conditions of our engagement to audit the financial
     statements and supplemental schedule of the Canadian Solar (USA) 401(k)
