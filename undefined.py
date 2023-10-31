425b9fc0a8aa4522ceec8dc0ac83a2ea1b8f1fea
Terry Li <terryseanli@gmail.com>
2023-10-31 16:52:15 -0700

    Makes label_undefined functional on terms of up to 3 tokens.

--- undefined.py
+++ undefined.py
@@ -7,21 +7,31 @@ from spacy.matcher import Matcher
 
 Doc.set_extension("undefined", default=[], force=True)
 
+
 def label_undefined(nlp, doc):
     """
     Finds and labels undefined terms, i.e., capitalized non-named entities that
     do not have a corresponding defined term declaration.
     """
-    patterns = [{"IS_TITLE": True, "OP": "+"}]
+    patterns = [
+        [{"IS_TITLE": True,
+          "IS_SENT_START": False,
+          "OP": "{1,3}"}]
+    ]
 
     matcher = Matcher(nlp.vocab)
     matcher.add("UndefinedTermsMatcher", patterns)
     matches = matcher(doc)
+    defterms_strings = [term.text for term in doc._.defterms]
     for match_id, start, end in matches:
-        span = doc[start, end]
-        if span not in doc._.defterms and span not in doc.ents:
+        span = doc[start:end]
+        no_tokens_in_entity = True
+        # Returns true if any token in span is part of an entity
+        for token in span:
+            if token.ent_type_:
+                no_tokens_in_entity = False
+        if (span.text not in defterms_strings) and no_tokens_in_entity:
             span.label_ = "UNDEFINED"
             doc._.undefined.append(span)
 
     print("undefined terms:", doc._.undefined)
-
